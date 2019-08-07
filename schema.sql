798ce6e6ff35262dc9ae498c88e8af481c02206d
Hun Lee <dan.hun.lee@gmail.com>
2019-08-07 01:29:56 -0700

    hl

---
+++ schema.sql
@@ -0,0 +1,20 @@
+-- Drops the boardGame_DB if it exists currently --
+DROP DATABASE IF EXISTS boardGame_DB;
+-- Creates the "boardGame_DB" database --
+CREATE DATABASE boardGame_DB;
+
+CREATE TABLE createBoardgames (
+  id int NOT NULL AUTO_INCREMENT,
+  gameTitle varchar(255) NOT NULL,
+  gameDescript varchar(500), NOT NULL,
+--   geoLocation varchar(50) NOT NULL,
+--   location varchar(50) NOT NULL,
+--   picture varchar(100) NOT NULL,
+--   maxOfPlayers int NOT NULL,
+--   phone int NOT NULL,
+--   userName_id int NOT NULL,
+--   playerUserNames_id int NOT NULL,
+  PRIMARY KEY (id),
+--   FOREIGN KEY (userName_id) REFERENCES user(id),
+--   FOREIGN KEY (playerUserNames_id) REFERENCES user(id)
+);
