558d293036f486f1d4e7c76bef703c7a1ea9cc04
Hun Lee <dan.hun.lee@gmail.com>
2019-08-12 12:26:23 -0700

    hl

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.use(express.static("public"));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", '*');
   // res.header("Access-Control-Allow-Credentials", true);
+  // force rebuild
   // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   // res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
