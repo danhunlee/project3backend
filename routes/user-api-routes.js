6227e50dbd680090e60c9bc70aca260f67635f78
Hun Lee <dan.hun.lee@gmail.com>
2019-08-13 10:26:58 -0700

    hl

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -133,4 +133,30 @@ module.exports = function(app) {
     });
   });
 
+    // PUT route for updating userProfile
+    app.put("/api/updateProfile/:id", function(req, res) {
+      db.User.update(
+        req.body,
+        {
+          where: {
+            id: req.params.id
+          }
+        });
+    });
+
+      // Get route for retrieving a single gameEvent
+  app.get("/api/getProfile/:id", function(req, res) {
+    // Here we add an "include" property to our options in our findOne query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.User
+    db.User.findOne({
+      where: {
+        id: req.params.id
+      },
+      // include: [db.User]
+    }).then(function(dbUser) {
+      res.json(dbUser);
+    });
+  });
+
 };
